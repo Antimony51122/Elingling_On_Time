48344534ea89e9be2c31814bcbb37062389edc8f
antimony51122 <zhuyaosheng51122@gmail.com>
2020-02-12 14:59:40 +0000

    start from scratch

--- Assets/Scripts/Gameplay/HoriMvtState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// An enumeration of the horizontal movement states
-public enum HoriMvtState {
-    Normal,
-    Buffed
-}

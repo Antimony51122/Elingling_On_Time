7f8d9678a9760d68c06f12c0c435a41575d6fb5b
antimony51122 <zhuyaosheng51122@gmail.com>
2020-02-14 21:56:47 +0000

    comments on z-pos functionality

--- Assets/Scripts/Gameplay/PlayerStatus.cs
+++ Assets/Scripts/Gameplay/PlayerStatus.cs
@@ -53,17 +53,23 @@ public class PlayerStatus : MonoBehaviour {
         Score = (int) transform.position.x + 5;
     }
 
-    // process trigger collisions with other game objects
+    // determine the player sprite's z-pos by comparing with the colliding vehicle objects
+    // this is to avoid the weird posture of the player sprite behind the vehicle sprite when
+    // the player looks actually closer to the Camera
     void OnTriggerEnter2D(Collider2D coll) {
+        // when the Player's center y-pos is lower than
+        // - Bus's center y-pos by more than 1 unit
+        // - Car's center y-pos by more than 0.25 units
         if ((coll.gameObject.name == "Bus(Clone)" &&
              transform.position.y < coll.transform.position.y - 1.0f) ||
             (coll.gameObject.name == "Car(Clone)" &&
              transform.position.y < coll.transform.position.y - 0.25f)) {
-            
+            // set z-pos of player to closer than the vehicles
             transform.position = new Vector3(transform.position.x, transform.position.y, -3);
         }
     }
 
+    // set the player's z-pos back to original when exit the vehicle's collider
     void OnTriggerExit2D(Collider2D coll) {
         Debug.Log(coll.gameObject.name);
         if (coll.gameObject.CompareTag("Vehicle")) {

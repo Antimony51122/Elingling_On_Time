374820e74fc51ded92f25645798340abeafad37c
antimony51122 <zhuyaosheng51122@gmail.com>
2020-02-14 23:53:29 +0000

    soldier chasing implemented

--- Assets/Scripts/Gameplay/SpawnedObjs/Soldier.cs
+++ Assets/Scripts/Gameplay/SpawnedObjs/Soldier.cs
@@ -8,9 +8,18 @@ public class Soldier : ZPosChangeable {
     // Field Variables
     // ======================================================================
 
+    // --------------- Serialized Cached References ---------------
+
     [SerializeField] private Animator _animator;
-    [SerializeField] private bool _isRunning;
-    
+    [SerializeField] private bool     _isRunning;
+
+    // --------------- Fields to be attached Component Instances ---------------
+
+    private Rigidbody2D _rb2D;
+
+    // --------------- Config Params ---------------
+
+    private float _impulseForce;
 
     // ======================================================================
     // Main Loop & MonoBehaviour Methods
@@ -20,11 +29,14 @@ public class Soldier : ZPosChangeable {
         // initialise soldier state with idle, thus _isRunning false
         _isRunning = false;
 
+        _rb2D = GetComponent<Rigidbody2D>();
 
+        _impulseForce = 8.5f;
     }
 
     void Update() {
         StartChasing();
+        Chasing();
     }
 
     // ======================================================================
@@ -38,5 +50,20 @@ public class Soldier : ZPosChangeable {
         }
     }
 
+    private void Chasing() {
+        if (_isRunning) {
+            _rb2D.velocity = Vector2.zero;
 
+            // calculate direction to the player and moving towards it
+            Vector2 direction = new Vector2(
+                PlayerControl.PlayerTransform.position.x - transform.position.x,
+                PlayerControl.PlayerTransform.position.y - transform.position.y);
+            direction.Normalize(); // normalise it to make it a unity vector
+
+            // because we set the speed to zero previously, adding the force with the original
+            // impulse force with the normalised direction we have just calculated will
+            // make the game object moving at the same speed as before
+            _rb2D.AddForce(direction * _impulseForce, ForceMode2D.Impulse);
+        }
+    }
 }

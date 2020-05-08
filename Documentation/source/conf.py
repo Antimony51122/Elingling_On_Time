e40833fca1d9f602e89c7f9878867cf7c7ad4295
antimony51122 <zhuyaosheng51122@gmail.com>
2020-05-08 22:13:27 +0100

    first commit, intro finished

--- Documentation/source/conf.py
+++ Documentation/source/conf.py
@@ -19,9 +19,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Elinging on Time[D[D[D[D[D[D[D[O'
-copyright = '2020, HRK'
-author = 'HRK'
+project = 'Elinging on Time'
+copyright = '2020, Renke Huang'
+author = 'Renke Huang'
 
 # The short X.Y version
 version = ''
@@ -39,6 +39,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -74,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -101,7 +102,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ElingingonTimeDDDDDDDOdoc'
+htmlhelp_basename = 'ElingingonTimedoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -128,7 +129,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ElingingonTimeDDDDDDDO.tex', 'Elinging on Time{[}D{[}D{[}D{[}D{[}D{[}D{[}D{[}O Documentation',
+    (master_doc, 'ElingingonTimeDDDDDDDO.tex', 'Elinging on Time Documentation',
      'HRK', 'manual'),
 ]
 
@@ -138,7 +139,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'elingingontimedddddddo', 'Elinging on Time[D[D[D[D[D[D[D[O Documentation',
+    (master_doc, 'elingingontimedddddddo', 'Elinging on Time Documentation',
      [author], 1)
 ]
 
@@ -149,7 +150,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ElingingonTimeDDDDDDDO', 'Elinging on Time[D[D[D[D[D[D[D[O Documentation',
+    (master_doc, 'ElingingonTimeDDDDDDDO', 'Elinging on Time Documentation',
      author, 'ElingingonTimeDDDDDDDO', 'One line description of project.',
      'Miscellaneous'),
 ]

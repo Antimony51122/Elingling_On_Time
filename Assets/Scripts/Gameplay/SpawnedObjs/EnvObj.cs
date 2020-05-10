5bb0406d546f4f11f943b3dcfa87f497cc13480e
antimony51122 <zhuyaosheng51122@gmail.com>
2020-05-10 15:53:43 +0100

    add the self-destroy functionality to environment objects without implementation

---
+++ Assets/Scripts/Gameplay/SpawnedObjs/EnvObj.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// just to add the self destroy functionality to the environment objects
+public class EnvObj : SpawnedObj {
+    // no implementations, just to avoid the problem caused by abstract classes
+    protected override void Start() { }
+
+    protected override void OnDestroy() { }
+}
